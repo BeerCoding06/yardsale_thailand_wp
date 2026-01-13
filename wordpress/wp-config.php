b3cd0983f49320e9a12c05fffcb55e068c5fa841
BeerCoding06 <paradon.pokpingmaung@gmail.com>
2026-01-13 17:36:41 +0700

    [update] pw

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'nuxtcommerce_db' );
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'KtmdoLt9b$n!' );
 
 /** Database hostname */
 define( 'DB_HOST', '157.85.98.150' );
